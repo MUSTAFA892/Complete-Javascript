318b9cb14fecf413bf47fc1291b3cf74510a920d
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-04-04 05:22:39 +0530

    from source 1 done up to varibles

---
+++ Source_1/Variables/script.js
@@ -0,0 +1,29 @@
+console.log("let")
+
+let a = "Piyush"
+console.log(a)
+
+a = 12
+console.log(a)
+
+if (true){
+    let b = "Mustafa"
+    console.log(b)
+}
+
+console.log(b)
+console .log('-----------')
+
+console.log("var")
+
+var c = "Mustafa"
+
+console.log(c)
+
+if (true){
+    console.log(c)
+}
+
+c = 13
+
+console.log(c)
